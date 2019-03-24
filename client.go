8013999b2e7b4c745bc8e325c73333fc771af51a
Martin Stidelius <martid@juppi.net>
2019-03-24 15:18:45 +0100

    Removed limit on GetVolumeInfo and GetAggregateInfo

--- client.go
+++ client.go
@@ -180,11 +180,11 @@ func (c *Client) GetDiskInfo() ([]DiskInfo, error) {
 	return ail, nil
 }
 
-func (c *Client) GetAggrInfo(limit int) ([]AggrInfo, error) {
+func (c *Client) GetAggrInfo() ([]AggrInfo, error) {
 	ixml := &AggrInfoRequest{}
 	ixml.Version = apiVersion
 	ixml.Xmlns = XMLns
-	ixml.AggrGetIter.MaxRecords = strconv.Itoa(limit)
+	//ixml.AggrGetIter.MaxRecords = strconv.Itoa(limit)
 
 	output, err := xml.MarshalIndent(ixml, "", "\t")
 
@@ -400,11 +400,11 @@ func (c *Client) getVolumeToAggrMap() (map[string]string, error) {
 }
 
 // Returns custom struct of Volumes Info
-func (c *Client) GetVolumeInfo(limit int) (volumes []VolumeInfo, err error) {
+func (c *Client) GetVolumeInfo() (volumes []VolumeInfo, err error) {
 	ixml := &VolumeGetIterShort{}
 	ixml.Version = apiVersion
 	ixml.Xmlns = XMLns
-	ixml.VolumeGetIter.MaxRecords = strconv.Itoa(limit)
+	//ixml.VolumeGetIter.MaxRecords = strconv.Itoa(limit)
 
 	output, err := xml.MarshalIndent(ixml, "", "\t")
 	if c.Debug {
